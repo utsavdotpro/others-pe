e3263d3a2babfa040ffb3ac06633431d340b4275
Utsav Barnwal <u.barnwal36@gmail.com>
2023-05-22 23:39:49 +0700

    update upi regex

--- src/core/utils/index.ts
+++ src/core/utils/index.ts
@@ -1,5 +1,5 @@
 export const validateUPIId = (upiId: string) => {
-  const upiIdRegex = /[a-zA-Z0-9.\-_]{2,49}@[a-zA-Z._]{2,49}/;
+  const upiIdRegex = /^[a-zA-Z0-9.\-_]{2,49}@[a-zA-Z._]{2,49}$/;
 
   console.log(upiIdRegex.test(upiId));
 
