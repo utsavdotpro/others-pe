8a5265ae307a5b6e63c7a94802b6f5042a404bc5
Utsav Barnwal <u.barnwal36@gmail.com>
2023-05-22 20:39:13 +0700

    refactor HomeScreen

---
+++ src/pages/HomeScreen.tsx
@@ -0,0 +1,11 @@
+import Screen from "@layouts/Screen";
+
+const HomeScreen: React.FC = () => {
+  return (
+    <Screen>
+      <div className="p-4 font-bold">Hello from Tailwind!</div>
+    </Screen>
+  );
+};
+
+export default HomeScreen;
