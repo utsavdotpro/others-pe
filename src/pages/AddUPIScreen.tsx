24135f803590c9ff4d0d90414e24709ecbe30659
Utsav Barnwal <u.barnwal36@gmail.com>
2023-05-22 23:04:21 +0700

    design & use Input element

--- src/pages/AddUPIScreen.tsx
+++ src/pages/AddUPIScreen.tsx
@@ -1,11 +1,17 @@
 import Button from "@elements/Button";
+import Input from "@elements/Input";
 import Container from "@layouts/Container";
 import Screen from "@layouts/Screen";
 
 const AddUPIScreen: React.FC = () => {
   return (
-    <Screen title="Add UPI Id" subtitle="Your friends UPI ID" toolbar>
+    <Screen title="Add new UPI Id" subtitle="Your friends UPI Id" toolbar>
       <Container>
+        <Input
+          placeholder="90876543@okicici"
+          errorText="Enter a correct UPI Id"
+        />
+
         <Button className="w-full mt-9" disabled>
           Add UPI ID
         </Button>
