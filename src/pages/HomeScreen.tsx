be624e431ab2e4111d9702e2b610f4713c510d55
Utsav Barnwal <u.barnwal36@gmail.com>
2023-05-23 21:30:51 +0700

    wip: UPIItem

--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -1,12 +1,22 @@
+import UPIItem from "@components/UPIItem";
+import Container from "@layouts/Container";
 import Screen from "@layouts/Screen";
+
+const mockList = [
+  {
+    upiId: "sannan@ybl",
+  },
+];
 
 const HomeScreen: React.FC = () => {
   return (
-    <Screen
-      title="Your UPI"
-      subtitle="Works with all the UPI IDs"
-      toolbar
-    ></Screen>
+    <Screen title="Your UPI" subtitle="Works with all the UPI IDs" toolbar>
+      <Container>
+        {mockList.map((item) => (
+          <UPIItem {...item} key={item.upiId} />
+        ))}
+      </Container>
+    </Screen>
   );
 };
 
