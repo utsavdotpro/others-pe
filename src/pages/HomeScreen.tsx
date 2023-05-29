3e9021c1fda89d0f2646561d7e813d4aacac0f47
Utsav Barnwal <u.barnwal36@gmail.com>
2023-05-29 21:46:43 +0700

    minor code refactor

--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -5,12 +5,29 @@ import Button from "@elements/Button";
 import Screen from "@layouts/Screen";
 import Section from "@layouts/Section";
 import Container from "@layouts/Container";
+import { Component } from "@appTypes/.";
+import UPIItem from "@components/UPIItem";
 
 const mockList = [
   {
     upiId: "sannan@ybl",
   },
 ];
+
+const EmptyUPIList: Component = () => (
+  <>
+    <Section.EmptyText className="mb-10 mt-14">
+      *Cricket noises*
+      <br />
+      <br />
+      Need help making some friends?
+    </Section.EmptyText>
+
+    <Button.Primary Icon={UserPlusIcon} className="!py-3 !px-7 mx-auto">
+      Add a UPI ID
+    </Button.Primary>
+  </>
+);
 
 const HomeScreen: React.FC = () => {
   return (
@@ -22,20 +39,11 @@ const HomeScreen: React.FC = () => {
         className="mb-8"
         action={{ text: "See All", fn: () => {} }}
       >
-        <Section.EmptyText className="mb-10 mt-14">
-          *Cricket noises*
-          <br />
-          <br />
-          Need help making some friends?
-        </Section.EmptyText>
+        {!mockList.length && <EmptyUPIList />}
 
-        <Button.Primary Icon={UserPlusIcon} className="!py-3 !px-7 mx-auto">
-          Add a UPI ID
-        </Button.Primary>
-
-        {/* {mockList.map((item) => (
+        {mockList.map((item) => (
           <UPIItem {...item} key={item.upiId} />
-        ))} */}
+        ))}
       </Section>
 
       <Section title="History" action={{ text: "See All", fn: () => {} }}>
