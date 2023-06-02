004a06359a0ad31ade8aee38d97f644efcbbe17c
Utsav Barnwal <u.barnwal36@gmail.com>
2023-06-02 11:27:09 +0700

    add functional skip button

--- src/common/components/Toolbar.tsx
+++ src/common/components/Toolbar.tsx
@@ -4,6 +4,8 @@ import Text from "@elements/Text";
 import { ArrowLeftIcon } from "@heroicons/react/24/solid";
 import cx from "clsx";
 import useRouter from "@hooks/use-router";
+import LocalStorage, { StorageItem } from "@lib/storage";
+import { useState } from "react";
 
 type Props = {
   title?: string;
@@ -12,6 +14,15 @@ type Props = {
 
 const Toolbar: Component<Props> = ({ title, subtitle, className }) => {
   const { goBack, canGoBack } = useRouter();
+  const [isOnboardingComplete, setIsOnboardingComplete] = useState(
+    LocalStorage.getBoolean(StorageItem.isOnboardingComplete)
+  );
+
+  const skipOnboarding = () => {
+    LocalStorage.setItem(StorageItem.isOnboardingComplete, "true");
+    setIsOnboardingComplete(true);
+  };
+
   return (
     <Container className={cx("flex items-center", className)}>
       {canGoBack() && (
@@ -28,6 +39,8 @@ const Toolbar: Component<Props> = ({ title, subtitle, className }) => {
 
         <Text className="text-xs">{subtitle}</Text>
       </div>
+
+      {!isOnboardingComplete && <Text onClick={skipOnboarding}>skip</Text>}
     </Container>
   );
 };
