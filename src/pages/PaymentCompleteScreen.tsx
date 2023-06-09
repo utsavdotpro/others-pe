87084b2973a17052a2e54525f69d93973938f044
Utsav Barnwal <u.barnwal36@gmail.com>
2023-06-09 20:15:00 +0700

    show user images

--- src/pages/PaymentCompleteScreen.tsx
+++ src/pages/PaymentCompleteScreen.tsx
@@ -1,15 +1,26 @@
+import { Component } from "@appTypes/.";
 import Toolbar from "@components/Toolbar";
 import Button from "@elements/Button";
+import Image from "@elements/Image";
 import Text from "@elements/Text";
 import Container from "@layouts/Container";
 import Screen from "@layouts/Screen";
 import { formatAmount } from "@utils/.";
+import cx from "clsx";
 
 const mockProps = {
-  payerUPIId: "utsav@ybl",
-  recipientUPIId: "sannan@ybl",
+  payerUPIId: "klprashant@icici",
+  recipientUPIId: "utsav.pro",
   amount: 50000,
 };
+
+const UserImage: Component<{ upiId: string }> = ({ className, upiId }) => (
+  <Image
+    src={`https://api.dicebear.com/6.x/initials/svg?seed=${upiId}&radius=50&fontFamily=Tahoma&fontSize=40&fontWeight=500`}
+    alt="recipient"
+    className={cx("w-20 h-20 rounded-full", className)}
+  />
+);
 
 const PaymentCompleteScreen: React.FC = () => {
   return (
@@ -18,7 +29,12 @@ const PaymentCompleteScreen: React.FC = () => {
 
       {/* 92px: space from the top with Toolbar */}
       <Container className="flex flex-col items-center min-h-[calc(100vh-92px-env(safe-area-inset-top))] text-center pb-7">
-        <Text>Request payment to</Text>
+        <div className="flex mb-6">
+          <UserImage className="opacity-80" upiId={mockProps.recipientUPIId} />
+          <UserImage className="z-50 -ml-6" upiId={mockProps.payerUPIId} />
+        </div>
+
+        <Text>Requested payment to</Text>
         <Text className="mt-2.5">{mockProps.recipientUPIId}</Text>
 
         <Text className="my-5 text-4xl">{formatAmount(mockProps.amount)}</Text>
