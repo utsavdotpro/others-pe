fd8d2e31609e1be5527612a9860afe2fdd59529c
Utsav Barnwal <u.barnwal36@gmail.com>
2023-06-11 15:57:21 +0700

    Enable Toggle Flash (#16)
    * android build
    
    * enable flash button
    
    * minor change

--- src/pages/ScannerScreen.tsx
+++ src/pages/ScannerScreen.tsx
@@ -5,7 +5,7 @@ import Screen from "@layouts/Screen";
 import { BoltIcon, PhotoIcon } from "@heroicons/react/24/outline";
 import { useEffect } from "react";
 import { Platform } from "@lib/platform";
-import { startQRScan, stopQRScan } from "@lib/barcode-scanner";
+import { startQRScan, stopQRScan, toggleFlash } from "@lib/barcode-scanner";
 import screen from "@constants/screens";
 import { useHistory } from "react-router";
 
@@ -51,8 +51,12 @@ const ScannerScreen: React.FC = () => {
         <TabBar
           className="mb-8"
           items={[
-            { label: "Upload QR", Icon: PhotoIcon },
-            { label: "Flash On", Icon: BoltIcon },
+            // { label: "Upload QR", Icon: PhotoIcon },
+            {
+              label: "Flash",
+              Icon: BoltIcon,
+              onClick: () => toggleFlash(),
+            },
           ]}
         />
       </Container>
