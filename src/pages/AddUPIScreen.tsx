f782a86e839c31335d43129142f1a052dfb7e196
Utsav Barnwal <u.barnwal36@gmail.com>
2023-06-15 22:48:20 +0700

    rename to localStorage

--- src/pages/AddUPIScreen.tsx
+++ src/pages/AddUPIScreen.tsx
@@ -4,7 +4,7 @@ import Text from "@elements/Text";
 import Input from "@elements/Input";
 import Container from "@layouts/Container";
 import Screen from "@layouts/Screen";
-import LocalStorage, { StorageItem } from "@lib/storage";
+import LocalStorage, { StorageItem } from "@lib/localStorage";
 import { debounce, validateUPIId } from "@utils/index";
 import { useRef, useState } from "react";
 import useRouter from "@hooks/use-router";
