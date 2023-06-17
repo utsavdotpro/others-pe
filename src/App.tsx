af00488a4ac343d684fbdaa05c892a5c90025717
Utsav Barnwal <u.barnwal36@gmail.com>
2023-06-17 11:39:37 +0700

    init amplitude

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,38 @@
+import * as amplitude from "@amplitude/analytics-browser";
+import screen from "@constants/screens";
 import { IonApp, IonRouterOutlet, setupIonicReact } from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
+import { useEffect } from "react";
 import { Redirect, Route } from "react-router-dom";
-import screen from "@constants/screens";
 
+import "./App.css";
 // Core CSS required for Ionic components to work properly
 import "@ionic/react/css/core.css";
 
-import "./App.css";
-
 setupIonicReact();
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        {Object.values(screen).map(({ name, path, Component }) => (
-          <Route exact path={path} key={name}>
-            <Component />
-          </Route>
-        ))}
+const App: React.FC = () => {
+  useEffect(() => {
+    amplitude.init(process.env.REACT_APP_AMPLITUDE_API_KEY!);
+  }, []);
 
-        <Route exact path="/">
-          <Redirect to={screen.home.path} />
-        </Route>
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
-);
+  return (
+    <IonApp>
+      <IonReactRouter>
+        <IonRouterOutlet>
+          {Object.values(screen).map(({ name, path, Component }) => (
+            <Route exact path={path} key={name}>
+              <Component />
+            </Route>
+          ))}
+
+          <Route exact path="/">
+            <Redirect to={screen.home.path} />
+          </Route>
+        </IonRouterOutlet>
+      </IonReactRouter>
+    </IonApp>
+  );
+};
 
 export default App;
